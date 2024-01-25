49596d8a4ad5127d8c95541ceb3a45b2d2ae2256
nianzx <371135341@nianzx.com>
2024-01-25 15:06:44 +0800

    commit

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,12 +29,76 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize) (this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize) (this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image) (resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(500, 500);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image) (resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(614, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(87, 64);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(614, 212);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "匹配";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(624, 364);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 12);
+            this.label1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 566);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize) (this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize) (this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -2,19 +2,135 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace opencv_lite_example
 {
-    public partial class Form1 : Form
+    public unsafe partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
+        }
+
+        enum DEPTH
+        {
+            DEPTH_8U = 0,
+            DEPTH_8S = 1,
+            DEPTH_16S = 2,
+            DEPTH_32S = 3,
+            DEPTH_32F = 4,
+            DEPTH_64F = 5,
+        };
+
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct TMatrix
+        {
+            public int Width; // The width of a matrix
+            public int Height; // The height of a matrix
+            public int WidthStep; // The number of bytes occupied by a line element of a matrix
+            public int Channel; // Number of matrix channels
+            public int Depth; // The type of matrix element
+            public byte* Data; // Data of a matrix
+            public int Reserved; // Reserved use
+
+            public TMatrix(int width, int height, int widthStep, int depth, int channel, byte* scan0)
+            {
+                Width = width;
+                Height = height;
+                WidthStep = widthStep;
+                Depth = depth;
+                Channel = channel;
+                Data = scan0;
+                Reserved = 0;
+            }
+        };
+
+
+        [DllImport("opencv-lite.dll")]
+        private static extern bool MatchTemplate(ref TMatrix src, ref TMatrix template, ref TMatrix* dest);
+
+        [DllImport("opencv-lite.dll")]
+        private static extern bool MinMaxLoc(ref TMatrix src, ref int minPosX, ref int minPosY, ref int maxPosX,
+            ref int maxPosY);
+
+        [DllImport("opencv-lite.dll")]
+        private static extern bool FreeMatrix(TMatrix** dstImg);
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int minPosX = 0, minPosY = 0, maxPosX = 0, maxPosY = 0;
+
+            var srcBmp = (Bitmap) pictureBox2.Image;
+            var dstBmp = (Bitmap) pictureBox1.Image;
+
+
+            var srcBmpData = srcBmp.LockBits(new Rectangle(0, 0, srcBmp.Width, srcBmp.Height),
+                ImageLockMode.ReadWrite, PixelFormat.Format24bppRgb);
+            var dstBmpData = dstBmp.LockBits(new Rectangle(0, 0, dstBmp.Width, dstBmp.Height),
+                ImageLockMode.ReadWrite, PixelFormat.Format24bppRgb);
+
+            var sw = new Stopwatch();
+            sw.Start();
+
+            var srcImg = new TMatrix(srcBmp.Width, srcBmp.Height, srcBmpData.Stride, (int) DEPTH.DEPTH_8U, 3,
+                (byte*) srcBmpData.Scan0);
+            var destImg = new TMatrix(dstBmp.Width, dstBmp.Height, dstBmpData.Stride, (int) DEPTH.DEPTH_8U, 3,
+                (byte*) dstBmpData.Scan0);
+
+            TMatrix* dest = null;
+            MatchTemplate(ref destImg, ref srcImg, ref dest);
+            MinMaxLoc(ref *dest, ref minPosX, ref minPosY, ref maxPosX, ref maxPosY);
+            FreeMatrix(&dest);
+            srcBmp.UnlockBits(srcBmpData);
+            dstBmp.UnlockBits(dstBmpData);
+
+            var P = new Pen(Color.Red);
+            var G = Graphics.FromImage(dstBmp);
+            G.DrawRectangle(P, new Rectangle(minPosX, minPosY, srcImg.Width, srcImg.Height));
+            P.Dispose();
+            G.Dispose();
+
+            label1.Text = @"匹配用时:" + sw.ElapsedMilliseconds + @" ms";
+            pictureBox1.Refresh();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                //获取嵌入dll文件的字节数组
+                var dll = Properties.Resources.opencv_lite;
+                //设置释放路径   导出路径
+                var strPath = @"./opencv-lite.dll";
+
+                var directoryPath = Path.GetDirectoryName(strPath);
+
+                // 检查文件夹是否存在，如果不存在则创建它  
+                if (!Directory.Exists(directoryPath))
+                {
+                    if (directoryPath != null) Directory.CreateDirectory(directoryPath);
+                }
+
+                //创建dll文件（覆盖模式）  
+                using (var fs = new FileStream(strPath, FileMode.Create))
+                {
+                    fs.Write(dll, 0, dll.Length);
+                }
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(@"释放opencv-lite.dll失败!");
+            }
         }
     }
 }
